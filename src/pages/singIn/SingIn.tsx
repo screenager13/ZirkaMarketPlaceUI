76d0dd11ce38cff3c0ca760b4c5af068189faa82
screenager <ydimaa.parfen@gmail.com>
2024-12-12 15:15:04 +0100

    update password validation

--- src/pages/singIn/SingIn.tsx
+++ src/pages/singIn/SingIn.tsx
@@ -29,6 +29,39 @@ const SingIn = () => {
         mode: 'all',
     });
 
+    const validatePassword = (password: string) => {
+        const minLength = 8;
+        const minLowercase = 1;
+        const minUppercase = 1;
+        const minNumbers = 1;
+        const minSymbols = 1;
+
+        if (password.length < minLength) {
+            return `Hasło musi mieć co najmniej ${minLength} znaków`;
+        }
+
+        const lowercaseCount = (password.match(/[a-z]/g) || []).length;
+        const uppercaseCount = (password.match(/[A-Z]/g) || []).length;
+        const numberCount = (password.match(/[0-9]/g) || []).length;
+        const symbolCount = (password.match(/[^a-zA-Z0-9]/g) || []).length;
+
+        if (lowercaseCount < minLowercase) {
+            return `Hasło musi zawierać co najmniej ${minLowercase} małą literę`;
+        }
+
+        if (uppercaseCount < minUppercase) {
+            return `Hasło musi zawierać co najmniej ${minUppercase} wielką literę`;
+        }
+
+        if (numberCount < minNumbers) {
+            return `Hasło musi zawierać co najmniej ${minNumbers} cyfrę`;
+        }
+
+        if (symbolCount < minSymbols) {
+            return `Hasło musi zawierać co najmniej ${minSymbols} symbol`;
+        }
+    }
+
     const handleSubmitLogin = async (data: LoginForm) => {
         try {
             await login(data).unwrap();
@@ -128,11 +161,7 @@ const SingIn = () => {
                                 control={control}
                                 rules={{
                                     required: 'Hasło jest wymagane',
-                                    minLength: {
-                                        value: 6,
-                                        message:
-                                            'Hasło musi mieć co najmniej 6 znaków',
-                                    },
+                                    validate: validatePassword,
                                 }}
                                 render={({ field, fieldState }) => (
                                     <TextField
