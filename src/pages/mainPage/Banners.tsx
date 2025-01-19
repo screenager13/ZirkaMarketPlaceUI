d32dd72471ced56c4c6363556a31f7f42ecb13db
screenager <ydimaa.parfen@gmail.com>
2025-01-19 21:06:33 +0100

    fix

--- src/pages/mainPage/Banners.tsx
+++ src/pages/mainPage/Banners.tsx
@@ -8,23 +8,23 @@ const Banner: React.FC = () => {
     const [activeIndex, setActiveIndex] = useState(0);
     const slides = [
         {
-            imageUrl: '../../../public/jakilaptop.png',
+            imageUrl: '/jakilaptop.png',
             Url: 'https://www.youtube.com/watch?v=-9C-sQTzaA8&ab_channel=JustJosh',
         },
         {
-            imageUrl: '../../../public/iphone.png',
+            imageUrl: '/iphone.png',
             Url: '/products',
         },
         {
-            imageUrl: '../../../public/odkurzac.png',
+            imageUrl: '/odkurzac.png',
             Url: '/products',
         },
         {
-            imageUrl: '../../../public/telewizor.png',
+            imageUrl: '/telewizor.png',
             Url: '/products',
         },
         {
-            imageUrl: '../../../public/monitor.png',
+            imageUrl: '/monitor.png',
             Url: '/products',
         },
     ];
